c4e314a44879337c852eb01867f9ce603e2854ec
elianbarea <bareaelian@gmail.com>
2021-06-21 14:34:57 -0300

    db y diagrama

---
+++ T.sql
@@ -0,0 +1,85 @@
+
+CREATE DATABASE TIENDA
+GO
+
+USE TIENDA
+
+
+CREATE TABLE Marcas(
+ID int  Primary key identity (1,1) not null,
+Nombre varchar (40) not null
+)
+
+
+CREATE TABLE Categorias(
+ID int  Primary key identity (1,1) not null,
+Nombre varchar (40) not null
+)
+
+
+CREATE TABLE Articulos(
+ID int  Primary key identity (1,1) not null,
+Nombre varchar (40) not null,
+Imagen varchar(300)NULL ,
+Descripcion varchar (50) NULL,
+Idmarca int  foreign key references Marcas (ID) not null ,
+Precio money not null ,
+Stock int not null default (0),
+Estado bit not null default (1),
+Idcategoria int foreign key references Categorias (ID) not null 
+)
+
+
+
+
+
+CREATE TABLE TipoUsuario(
+Tipo smallint primary key not null default (1),
+Nombre VARCHAR (30) NOT NULL
+)
+
+
+CREATE TABLE Usuario(
+Mail varchar (40) primary key ,
+Contraseña varchar (15) not null,
+tipo smallint foreign key references TipoUsuario (Tipo)
+)
+
+
+CREATE TABLE DetalleUsuarios(
+DNI VARCHAR (8) primary key NOT NULL ,
+Mail varchar (40) foreign key references Usuario (Mail),
+Nombre VARCHAR (30) NOT NULL,
+Direccion VARCHAR (40) NOT NULL,
+Telefono VARCHAR (20) NOT NULL,
+)
+
+CREATE TABLE Carrito (
+DNIpersona varchar(8) NOT NULL FOREIGN KEY REFERENCES DetalleUsuarios (DNI),
+Subtotal money not null,
+IdArticulo int foreign key references Articulos (ID),
+cantidad varchar (5) not null,
+)
+
+CREATE TABLE DETALLE_COMPRA(
+OrdenCompra varchar (10) primary key,
+Idarticulo int foreign key references Articulos (ID),
+Abonado money not null,
+Envio varchar (200) not null,
+Estado bit not null default (1),
+Fecha date not null,
+Dni varchar (8) NOT NULL FOREIGN KEY REFERENCES DetalleUsuarios (DNI),
+)
+
+insert into Marcas values ('EVO POTENZA')
+insert into Categorias values ('Memorias ram')
+Insert into Articulos (Nombre, Imagen, Descripcion,Idmarca,Precio,Stock,Idcategoria)
+Values ('Memoria GeiL DDR4 8GB EVO POTENZA RED ', 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_17097_Memoria_GeiL_DDR4_8GB_3000MHz_EVO_POTENZA_RED_469a5a9c-grn.jpg',
+' 3000MHz',1 , 5950,2 ,1)
+
+insert into Marcas values ('Gigabyte ')
+insert into Categorias values ('Mothers')
+Insert into Articulos (Nombre, Imagen, Descripcion,Idmarca,Precio,Stock,Idcategoria)
+Values ('Mother Gigabyte H310M-H LGA 1151 ', 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_7715_Mother_Gigabyte_H310M-H_LGA_1151_8va_gen__bdf6e65f-grn.jpg',
+' Plataforma Intel, Socket 1151 Coffe Lake,  ',3 , 6000,4 ,2)
+
